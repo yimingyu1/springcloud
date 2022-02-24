6fb01dc66df6a2b9e6f0b4808fad7e70d58ef538
yimingyu1 <371044760@qq.com>
2022-02-24 08:52:30 +0800

    hystrix dashboard

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/mingyu/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,27 @@
+package com.mingyu.springcloud;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * ClassName: HystrixDashboardMain9001
+ * Description:
+ * date: 2022/2/24 08:36
+ *
+ * @author yimingyu
+ * @version 1.0
+ * @since JDK 1.8
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+
+
+}
--- cloud-provider-payment8001/src/main/java/com/mingyu/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/mingyu/springcloud/PaymentMain8001.java
@@ -24,4 +24,6 @@ public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
     }
+
+
 }
